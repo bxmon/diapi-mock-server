d5a0db5adf3f8be89da7a28228c2870fea5e9d09
Le Trong <le.trong@bxmon.com>
2019-06-15 12:59:58 -0400

    Implemented user controllers + AddUserHandler + GetUsersHandler + GetUserByIDHandler + UpdateUserHandler + DeleteUserByIDHandler

--- controller/controller.go
+++ controller/controller.go
@@ -1,6 +1,9 @@
 package controller
 
 import (
+	"net/http"
+
+	"github.com/bxmon/diapi-mock-server/model"
 	"github.com/bxmon/diapi-mock-server/service"
 	"github.com/gin-gonic/gin"
 )
@@ -17,24 +20,94 @@ func NewController(service *service.Service) *Controller {
 
 // AddUserHandler handles add new user action
 func (c *Controller) AddUserHandler(cx *gin.Context) {
+	reqUser := new(model.User)
+	if err := cx.ShouldBind(reqUser); err != nil {
+		cx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "Cannot bind request body"})
+		return
+	}
+
+	if err := c.service.AddNewUser(reqUser); err != nil {
+		cx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "Cannot add new user"})
+		return
+	}
+
+	cx.JSON(http.StatusOK, gin.H{"userDetails": reqUser})
 }
 
 // GetUsersHandler handles get users action
 func (c *Controller) GetUsersHandler(cx *gin.Context) {
+	users, err := c.service.GetAllUsers()
+	if err != nil {
+		cx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "Cannot get all users"})
+		return
+	}
+
+	cx.JSON(http.StatusOK, gin.H{"users": users})
 }
 
 // GetUserByIDHandler handles get user by id action
 func (c *Controller) GetUserByIDHandler(cx *gin.Context) {
+	params := struct {
+		ID int `uri:"userid" binding:"required"`
+	}{}
+
+	err := cx.ShouldBindUri(&params)
+	if err != nil {
+		cx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "Cannot bind request params"})
+		return
+	}
+
+	user, err := c.service.GetUserByID(params.ID)
+	if err != nil {
+		cx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "Cannot get user"})
+		return
+	}
+
+	cx.JSON(http.StatusOK, gin.H{"userDetails": user})
 }
 
 // UpdateUserHandler handles update user action
 func (c *Controller) UpdateUserHandler(cx *gin.Context) {
-}
+	params := struct {
+		ID int `uri:"userid" binding:"required"`
+	}{}
 
-// ReplaceUserHandler handles update user action
-func (c *Controller) ReplaceUserHandler(cx *gin.Context) {
+	err := cx.ShouldBindUri(&params)
+	if err != nil {
+		cx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "Cannot bind request params"})
+		return
+	}
+
+	reqUser := new(model.User)
+	if err := cx.ShouldBind(reqUser); err != nil {
+		cx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "Cannot bind request body"})
+		return
+	}
+
+	if err := c.service.UpdateUser(reqUser); err != nil {
+		cx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "Cannot update user"})
+		return
+	}
+
+	cx.JSON(http.StatusOK, gin.H{"userDetails": reqUser})
 }
 
 // DeleteUserByIDHandler handles delete user action
 func (c *Controller) DeleteUserByIDHandler(cx *gin.Context) {
+	params := struct {
+		ID int `uri:"userid" binding:"required"`
+	}{}
+
+	err := cx.ShouldBindUri(&params)
+	if err != nil {
+		cx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "Cannot bind request params"})
+		return
+	}
+
+	if err := c.service.DeleteUserByID(params.ID); err != nil {
+		cx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "Cannot delete user"})
+		return
+	}
+
+	cx.JSON(http.StatusOK, gin.H{"message": "Success"})
 }
